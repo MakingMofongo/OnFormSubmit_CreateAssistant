99265e7f038e9df0b27d3512f9b925ac05c7c711
Abdul Rasheed Esa <rasheed3086@gmail.com>
2024-08-15 16:46:46 +0530

    added /deploy to url

--- main.js
+++ main.js
@@ -87,7 +87,7 @@ app.post('/', async (req, res) => {
 
         // send post request to OnAsstIdGenerated server with assistant id
         try {
-            const response = await fetch('https://createinstanceofassistant1-5a4aan2gca-el.a.run.app', {
+            const response = await fetch('https://createinstanceofassistant1-5a4aan2gca-el.a.run.app/deploy', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
